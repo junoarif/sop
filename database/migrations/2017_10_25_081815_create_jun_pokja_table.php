7a048d8079bb6b4a6abcc31e4c6528b32242e3b3
Juno Arif <arifherjuno@gmail.com>
2017-10-25 15:24:11 +0700

    add migration jun_pokja table

---
+++ database/migrations/2017_10_25_081815_create_jun_pokja_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJunPokjaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jun_pokja', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jun_pokja');
+    }
+}
